4fb8c48b7e2a8f64cf4750522d299cf24ae8581c
Mikus Riekstins <mikus@mikus-mac.lan>
2018-07-22 23:41:04 +0300

    Card layout and illustrations

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 
-let card = document.querySelector('.plate');
+let cards = document.querySelectorAll('.card');
 let over = false;
+let mouseMove = false;
 
-card.addEventListener('mouseover', function(e) {
-  over = true;
-  console.log("in");
-});
+for (var i=0; i<cards.length; i++) {
+  let card = cards[i];
 
-card.addEventListener('mouseout', function(e) {
-  over = false;
-  console.log("out");
-});
+  card.addEventListener('mouseover', function(e) {
+    over = card;
+  });
 
-window.addEventListener('mousemove', function(e) {
-  if( over) {
-    let xAxis = (card.offsetWidth - (e.pageX - card.offsetLeft + card.offsetWidth/2)) / 40;
-    let yAxis = (card.offsetHeight - (e.pageY - card.offsetTop + card.offsetHeight/2)) / -30;
-    card.style.transform = 'translateZ(100px) perspective(2000px) rotateY('+ xAxis +'deg) rotateX('+ yAxis +'deg)';
-  } else {
-    card.style.transform = 'translateZ(100px) perspective(2000px) rotateY(0) rotateX(0)';
+  card.addEventListener('mouseout', function(e) {
+    over.style.transform = 'translateZ(100px) perspective(2000px) rotateY(0) rotateX(0)';
+    over = false;
+  });
+}
+
+window.addEventListener('mousemove', function (e) {
+  if(over) {
+    let xAxis = (over.offsetWidth - (e.pageX - over.offsetLeft + over.offsetWidth/2)) / 40;
+    let yAxis = (over.offsetHeight - (e.pageY - over.offsetTop + over.offsetHeight/2)) / -30;
+    over.style.transform = 'translateZ(100px) perspective(2000px) rotateY('+ xAxis +'deg) rotateX('+ yAxis +'deg)';
   }
 });
